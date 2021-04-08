0d559dfba5640f2fa7d2b9a036fb584becebf887
jhimanshu623 <jhimanshu623@gmail.com>
2021-04-09 00:52:49 +0530

    done 3 upto 18

---
+++ ladder_25/18_DrazilAndDate.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }
+    public static void main(String[] args) {
+        FastReader sc=new FastReader(); 
+        
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int s=sc.nextInt();
+        int ms=Math.abs(a)+Math.abs(b);
+        if(s<ms){
+            System.out.println("No");
+        }else if((s-ms)%2==0){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
+
